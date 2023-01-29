29f8cbf95ac0456f768893c5a4da299c9366c6fe
Freud <foulon.wpi>
2023-01-29 18:04:58 -0500

    removed realsense lib

--- third-party/tracking_cam/test/TCam_test.cpp
+++ third-party/tracking_cam/test/TCam_test.cpp
@@ -5,11 +5,15 @@
 int main(int argc, char** argv) {
 
     TCam tCam;
-    tCam.init();
-    while(true){
-        tCam.get_pose();
-        printf(" position: %.3f %.3f %.3f\n", tCam.pose.position[0], tCam.pose.position[1], tCam.pose.position[2]);
+    
+    if(tCam.init()){
+        while(true){
+            tCam.get_pose();
+            printf(" position: %.3f %.3f %.3f\n", tCam.pose.position[0], tCam.pose.position[1], tCam.pose.position[2]);
+    }
     }
     
-    
+    else {
+        printf("no cam connected");
+    }   
 }
